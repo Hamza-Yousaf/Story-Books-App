0389619a667da4a370b993627589f0ba9211c02d
Hamza-Yousaf <Hamzayousaf933@gmail.com>
2019-03-19 23:36:33 +0500

    comment functionality & block private story

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI:'mongodb://Hamza:933786vpn@ds233571.mlab.com:33571/my-storybooks-dev',
-  googleClientID:'191664545271-olmdcs7j8uvrsqg30e3fj5fdqu6qfusa.apps.googleusercontent.com',
-  googleClientSecret:'ICWBMcAxwSCJI-r2H1BFYinV'
-}
+  mongoURI:
+    'mongodb://Hamza:933786vpn@ds233571.mlab.com:33571/my-storybooks-dev',
+  googleClientID:
+    '191664545271-olmdcs7j8uvrsqg30e3fj5fdqu6qfusa.apps.googleusercontent.com',
+  googleClientSecret: 'ICWBMcAxwSCJI-r2H1BFYinV'
+};
